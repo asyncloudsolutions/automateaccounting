51627cb11a64d393850ede6ab7556d164e1512b6
satwik <satwik.dondapatii77@gmail.com>
2017-08-16 23:19:41 +0530

    providers commited satwik..

--- AAA.DataConversions/Converters/TransactionConverter.cs
+++ AAA.DataConversions/Converters/TransactionConverter.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AAA.DataModels;
+using AAA.Entities;
 
 namespace AAA.DataConversions
 {
     public static class TransactionConverter
     {
+        
+        #region BankReconciliation
+        public static BankReconciliation ToBankReconciliation(this BaseBankReconciliationModel bankReconciliation)
+        {
+            BankReconciliation _result = new BankReconciliation();
+            if (user.Id.HasValue) { _result.Id = user.Id.GetValueOrDefault(); }
+            _result.LedgerPostingId = bankReconciliation.LedgerPostingId;
+            _result.StatementDate = bankReconciliation.StatementDate;
+            
+            return _result;
+        }
+        #endregion
+
+        #region Detail
+        public static Detail ToDetail(this BaseDetailModel detail)
+        {
+            Detail _result = new Detail();
+            if (detail.Id.HasValue) { _result.Id = detail.Id.GetValueOrDefault(); }
+            _result.MasterId = detail.MasterId;
+              _result.Name = detail.Name;
+              _result.Text = detail.Text;
+              _result.Row = detail.Row;
+              _result.Columns = detail.Columns;
+              _result.Width = detail.Width;
+              _result.DBF = detail.DBF;
+              _result.DorH = detail.DorH;
+              _result.Repeat = detail.Repeat;
+             _result.Align = detail.Align;
+              _result.RepeatAll = detail.RepeatAll;
+            _result.FooterRepeatAll = detail.FooterRepeatAll;
+              _result.TextWrap = detail.TextWrap;
+             _result.WrapLineCount = detail.WrapLineCount;
+              _result.ExtraFieldName = detail.ExtraFieldName;
+                          _result.FieldsForExtra = detail.FieldsForExtra;
+            
+            
+            
+            return _result;
+        }
+        #endregion
+
+        #region LedgerPosting
+        public static LedgerPosting ToLedgerPosting(this BaseLedgerPostingModel ledgerPosting)
+        {
+            LedgerPosting _result = new LedgerPosting();
+            if (ledgerPosting.Id.HasValue) { _result.Id = ledgerPosting.Id.GetValueOrDefault(); }
+            _result.Date = ledgerPosting.Date;
+            _result.VoucherTypeId = ledgerPosting.VoucherTypeId;
+            _result.LedgerId = ledgerPosting.LedgerId;
+            _result.Debit = ledgerPosting.Debit;
+            _result.Credit = ledgerPosting.Credit;
+            _result.DetailsId = ledgerPosting.DetailsId;
+            _result.FinancialYearId = ledgerPosting.FinancialYearId;
+            _result.InvoiceNumber = ledgerPosting.InvoiceNumber;
+            _result.Text = ledgerPosting.Text;
+            _result.ChequeNumber = ledgerPosting.ChequeNumber;
+            _result.ChequeDate = ledgerPosting.ChequeDate;
+                     
+            return _result;
+        }
+        #endregion
     }
 }
--- AAA.DataProviders/Contracts/IMasterProvider.cs
+++ AAA.DataProviders/Contracts/IMasterProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AAA.ReqResp;
+using AAA.ReqResp.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,19 +11,231 @@ namespace AAA.DataProviders
     public interface IMasterProvider
     {
         #region Master
+        #region Get
+        Task<GetBaseMastersRequest> GetBaseMastersAsync(GetBaseMastersRequest request);
+        #endregion
 
+        #region Upsert
+        Task<UpsertBaseMasterResponse> UpsertBaseMasterAsync(UpsertBaseMasterRequest request);
+        #endregion
         #endregion
 
         #region Currency
-
+        #region Get
+        Task<GetBaseCurrenciesResponse> GetBaseCurrenciesAsync(GetBaseCurrenciesRequest request);
         #endregion
 
+        #region Upsert
+        Task<UpsertBaseCurrencyResponse> UpsertBaseCurrencyAsync(UpsertBaseCurrencyRequest request);
+        #endregion
+        #endregion
+
+       
         #region Tax
-
+        #region Get
+        Task<GetBaseTaxesResponse> GetBaseTaxesAsync(GetBaseTaxesRequest request);
         #endregion
 
-        #region VoucherType
+        #region Upsert
+        Task<UpsertBaseTaxResponse> UpsertBaseTaxAsync(UpsertBaseTaxRequest request);
+        #endregion
+        #endregion
 
+        
+        #region VoucherType
+        #region Get
+        Task<GetBaseVoucherTypesResponse> GetBaseVoucherTypesAsync(GetBaseVoucherTypesRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseVoucherTypeResponse> UpsertBaseVoucherTypeAsync(UpsertBaseVoucherTypeRequest request);
+        #endregion
+        #endregion
+
+        
+        #region AccountGroup
+        #region Get
+        Task<GetBaseAccountGroupsResponse> GetBaseAccountGroupsAsync(GetBaseAccountGroupsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseAccountGroupResponse> UpsertBaseAccountGroupAsync(UpsertBaseAccountGroupRequest request);
+        #endregion
+        #endregion
+
+        
+        #region AccountLedger
+        #region Get
+        Task<GetBaseAccountLedgersResponse> GetBaseAccountLedgersAsync(GetBaseAccountLedgersRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseAccountLedgerResponse> UpsertBaseAccountLedgerAsync(UpsertBaseAccountLedgerRequest request);
+        #endregion
+        #endregion
+
+        
+        #region AdditionalCost
+        #region Get
+        Task<GetBaseAdditionalCostsResponse> GetBaseAdditionalCostsAsync(GetBaseAdditionalCostsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseAdditionalCostResponse> UpsertBaseAdditionalCostAsync(UpsertBaseAdditionalCostRequest request);
+        #endregion
+        #endregion
+
+        
+        #region AdvancePayment
+        #region Get
+        Task<GetBaseAdvancePaymentsResponse> GetBaseAdvancePaymentsAsync(GetBaseAdvancePaymentsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseAdvancePaymentResponse> UpsertBaseAdvancePaymentAsync(UpsertBaseAdvancePaymentRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Area
+        #region Get
+        Task<GetBaseAreasResponse> GetBaseAreasAsync(GetBaseAreasRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseAreaResponse> UpsertBaseAreaAsync(UpsertBaseAreaRequest request);
+        #endregion
+        #endregion
+
+        
+        #region  Batch
+        #region Get
+        Task<GetBaseBatchesResponse> GetBaseBatchesAsync(GetBaseBatchesRequestrequest);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseBatchResponse> UpsertBaseBatchAsync(UpsertBaseBatchRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Brand
+        #region Get
+        Task<GetBaseBrandsResponse> GetBaseBrandsAsync(GetBaseBrandsRequest request);
+        #endregion
+
+        #region UpsertBrandResponse> UpsertBaseBrandAsync(UpsertBaseBrandRequest request);
+        #endregion
+        #endregion
+
+        
+        #region ExchangeRate
+        #region Get
+        Task<GetBaseExchangeRatesResponse> GetBaseExchangeRatesAsync(GetBaseExchangeRatesRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseExchangeRateResponse> UpsertBaseExchangeRateAsync(UpsertBaseExchangeRateRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Godown
+        #region Get
+        Task<GetBaseGodownsResponse> GetBaseGodownsAsync(GetBaseGodownsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseGodownResponse> UpsertBaseGodownAsync(UpsertBaseGodownRequest request);
+        #endregion
+        #endregion
+
+        
+        #region ModelNumber
+        #region Get
+        Task<GetBaseModelNumbersResponse> GetBaseModelNumbersAsync(GetBaseModelNumbersRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseModelNumberResponse> UpsertBaseModelNumberAsync(UpsertBaseModelNumberRequest request);
+        #endregion
+        #endregion
+
+        
+        #region PricingLevel
+        #region Get
+        Task<GetBasePricingLevelsResponse> GetBasePricingLevelsAsync(GetBasePricingLevelsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBasePricingLevelResponse> UpsertBasePricingLevelAsync(UpsertBasePricingLevelRequest request);
+        #endregion
+        #endregion
+
+        
+        #region ProductGroup
+        #region Get
+        Task<GetBaseProductGroupsResponse> GetBaseProductGroupsAsync(GetBaseProductGroupsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseProductGroupResponse> UpsertBaseProductGroupAsync(UpsertBaseProductGroupRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Product
+        #region Get
+        Task<GetBaseProductsResponse> GetBaseProductsAsync(GetBaseProductsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseProductResponse> UpsertBaseProductAsync(UpsertBaseProductRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Rack
+        #region Get
+        Task<GetBaseRacksResponse> GetBaseRacksAsync(GetBaseRacksRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseRackResponse> UpsertBaseRackAsync(UpsertBaseRackRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Route
+        #region Get
+        Task<GetBaseRoutesResponse> GetBaseRoutesAsync(GetBaseRoutesRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseRouteResponse> UpsertBaseRouteAsync(UpsertBaseRouteRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Size
+        #region Get
+        Task<GetBaseSizesResponse> GetBaseSizesAsync(GetBaseSizesRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseSizeResponse> UpsertBaseSizeAsync(UpsertBaseSizeRequest request);
+        #endregion
+        #endregion
+
+        
+        #region Unit
+        #region Get
+        Task<GetBaseUnitsResponse> GetBaseUnitsAsync(GetBaseUnitsRequest request);
+        #endregion
+
+        #region Upsert
+        Task<UpsertBaseUnitResponse> UpsertBaseUnitAsync(UpsertBaseUnitRequest request);
+        #endregion
         #endregion
     }
 }
